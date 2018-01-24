8c7b4dede595e6f1b7a6ab9800278c29ded4c743
Unknown <patryk9595@gmail.com>
2018-01-24 22:43:52 +0100

    Added neighbors list to gems

---
+++ Assets/Scripts/GemControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemControler : MonoBehaviour
+{
+    public List<GameObject> neighbors;
+
+    private void Awake()
+    {
+        neighbors = new List<GameObject>();
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.isTrigger)
+        {
+            neighbors.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.isTrigger)
+        {
+            neighbors.Remove(collision.gameObject);
+        }
+    }
+}
